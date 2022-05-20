0e970ddef99b14cb6aa25f2f31fece81ea93165f
kauhguerra <105939640+kauhguerra@users.noreply.github.com>
2022-05-20 13:47:06 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+
+var box
+
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite (200,200,50,50);
+
+}
+
+function draw() 
+{
+
+  background(0);
+
+if (keyIsDown(RIGHT_ARROW))
+{
+box.position.x += 5;
+}
+
+if (keyIsDown(LEFT_ARROW))
+{
+box.position.x -= 5;
+}
+
+if (keyIsDown(UP_ARROW))
+{
+box.position.y -= 5;
+}
+
+if (keyIsDown(DOWN_ARROW))
+{
+box.position.y += 5;
+}
+
+
+
+drawSprites();
+}
+
+
+
+
